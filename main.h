8f3c1eea458b8b97b8212a644478edbc5164e430
OsiroA <osiroasunde@gmail.com>
2023-03-27 12:29:22 +0100

    This function turns on flags if printf finds it

--- main.h
+++ main.h
@@ -12,5 +12,6 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *str);
 char *convert(unsigned long int num, int base, int lowercase);
-
+int get_flag(char s, flags_t *f);
+int (*get_print(char s))(va_list, flags_t *);
 #endif

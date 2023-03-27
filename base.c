2f54c019b907707389ad6fd18ad5c0bfb1f52ca8
Bide1101 <olabiyibisolad@gmail.com>
2023-03-27 04:25:47 -0700

    prints base

---
+++ base.c
@@ -0,0 +1,75 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * print_binary - prints a number in base 2
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct
+ * Return: the number of char printed
+ */
+
+int print_binary(va_list l, flags_t *f)
+{
+        unsigned int n = va_arg(l, unsigned int);
+        char *str = convert(n, 2, 0);
+
+        (void)f;
+        return (_puts(str));
+}
+
+/**
+ * print_octal - prints a number in base 8
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct
+ * Return: the number of char printed
+ */
+
+int print_octal(va_list l, flags_t *f)
+{
+        unsigned int n = va_arg(l, unsigned int);
+        char *str = convert(n, 8, 0);
+        int c = 0;
+
+        if (f->hash == 1 && str[0] != '0')
+                c += _putchar('0');
+        c += _puts(str);
+        return (c);
+}
+
+/**
+ * print_hex - prints a number in hexadecimal base in lowercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags
+ * Return: the number of char printed
+ */
+
+int print_hex(va_list l, flags_t *f)
+{
+	unsigned int n = va_arg(l, unsigned int);
+	char *str = convert(n, 16, 1);
+	int c = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		c += _puts("0x");
+	c += _puts(str);
+	return (c);
+}
+
+/**
+ * print_hex_big - prints a number in hexadecimal base in uppercase
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct
+ * Return: the number of char printed
+ */
+
+int print_hex_big(va_list l, flags_t *f)
+{
+	unsigned int n = va_arg(l, unsigned int);
+	char *str = convert(n, 16, 0);
+	int c = 0;
+
+	if (f->hash == 1 && str[0] != '0')
+		c += _puts("0X");
+	c += _puts(str);
+	return (c);
+}
--- main.h
+++ main.h
@@ -3,6 +3,10 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
+int print_binary(va_list l, flags_t *f);
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
 int print_address(va_list l, flags_t *f);
 int _printf(const char *format, ...);
 int _putchar(char c);

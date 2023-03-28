e725759c2633b44ecb14bba6e076b24528c6d81c
Bide1101 <olabiyibisolad@gmail.com>
2023-03-28 06:46:30 -0700

    fix error in precision for non-custom conversion specifiers function

--- gets.c
+++ gets.c
@@ -3,40 +3,49 @@
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
+ * @i: Index of the current character in the format string
  * @list: list of arguments.
- *
  * Return: Precision.
  */
+
 int get_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
+	int curr_i = *i;
 	int precision = -1;
 
-	if (format[curr_i] != '.')
-	return (precision);
-
-	precision = 0;
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	while (format[curr_i] != '\0')
 	{
-		if (is_digit(format[curr_i]))
+		if (format[curr_i] == '.')
+		{
+			precision = 0;
+			break;
+		}
+		curr_i++;
+	}
+
+	if (precision == -1)
+	{
+		return (precision);
+	}
+
+	curr_i++;
+
+	if (format[curr_i] == '*')
+	{
+		precision = va_arg(list, int);
+		curr_i++;
+	}
+	else
+	{
+		while (is_digit(format[curr_i]))
 		{
 			precision *= 10;
 			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
 			curr_i++;
-			precision = va_arg(list, int);
-			break;
 		}
-		else
-			break;
 	}
 
-	*i = curr_i - 1;
-
+	*i = curr_i;
 	return (precision);
 }
 
